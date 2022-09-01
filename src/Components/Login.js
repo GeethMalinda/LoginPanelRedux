d1ea3c095678210120b3f59b2ba9b51e0b2c3b4b
geeth <geeth@misynergy.com>
2022-09-02 02:47:13 +0530

    login

---
+++ src/Components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className={login}>
+            <form className={login_form}>
+                <h1>Login Here</h1>
+                <input type="name" placeholder= "Name"/>
+                <input type="email" placeholder= "Email"/>
+                <input type="password" placeholder= "Passward"/>
+            </form>
+        </div>
+    )
+}
+export default Login
