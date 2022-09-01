1cdd6953a7cad35b704e200138828e63b724709c
geeth <geeth@misynergy.com>
2022-09-02 02:56:43 +0530

    two way binding

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Login = () => {
+    const [name ,setName] = useState('');
+    const [email ,setEmail] = useState('');
+    const [passward ,setPassward] = useState('');
+
     return (
         <div className={login}>
             <form className={login_form}>
                 <h1>Login Here</h1>
-                <input type="name" placeholder= "Name"/>
-                <input type="email" placeholder= "Email"/>
-                <input type="password" placeholder= "Passward"/>
+                <input type="name"
+                       placeholder= "Name"
+                       onChange={(event)=> setName(event.target.value)}
+                       value={name}
+                />
+                <input type="email"
+                       placeholder= "Email"
+                       onChange={(event) => setEmail((event.target.value))}
+                       value={email}
+                />
+                <input type="password"
+                       placeholder= "Passward"
+                       onChange={(event) =>setPassward(event.target.value)}
+                       value={passward}
+                />
             </form>
         </div>
     )
