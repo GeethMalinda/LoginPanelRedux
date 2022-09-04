2571ac31b222db7a11777e02b31665a90811f893
geeth <geeth@misynergy.com>
2022-09-04 16:40:12 +0530

    improvement

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,13 +1,29 @@
 import React, {useState} from 'react';
 import "./Login.css"
+import {useDispatch} from 'react-redux';
+import {login} from '../features/userSlice';
 const Login = () => {
     const [name ,setName] = useState('');
     const [email ,setEmail] = useState('');
     const [passward ,setPassward] = useState('');
 
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        console.log(e);
+        e.preventDefault();
+        dispatch(
+            login({
+                name:name,
+                email:email,
+                passward:passward,
+                loggedIn:true
+            }))
+    }
+
     return (
         <div className="login">
-            <form className="login_form">
+            <form className="login_form" onSubmit={(event => handleSubmit(event))}>
                 <h1>Login Here</h1>
                 <input type="name"
                        placeholder= "Name"
--- src/Components/Logout.js
+++ src/Components/Logout.js
@@ -1,14 +1,23 @@
-import React, {useState} from 'react';
-import "./Login.css"
-const Logout = () => {
+import React from 'react';
+import './LogOut.css';
+import {useDispatch, useSelector} from 'react-redux';
+import {logout, selectUser} from '../features/userSlice';
 
+const Logout = () => {
+    const dispatch = useDispatch();
+    const user = useSelector(selectUser)
+    const handleLogOut = (e) => {
+        e.preventDefault();
+        dispatch(logout())
+    }
     return (
-        <div>
+        <div className='logout' >
             <h1>
-                Welcome <span className="user_name">Geeth</span>
-                <button className="logout_button"> Log out</button>
+                Welcome <span className="user_name">{user.name}</span>
+
             </h1>
+            <button className="logout_button" onClick={(event => handleLogOut(event))}> Log out</button>
         </div>
-    )
-}
-export default Logout
+    );
+};
+export default Logout;
